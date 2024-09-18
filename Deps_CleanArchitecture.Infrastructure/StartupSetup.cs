d24eaa397b5510010ac90303108fabbaca4f71f6
Miguel Soeth <61883339+miguelsoeth@users.noreply.github.com>
2024-09-18 17:11:35 -0300

    Revert "V1"

--- Deps_CleanArchitecture.Infrastructure/StartupSetup.cs
+++ Deps_CleanArchitecture.Infrastructure/StartupSetup.cs
@@ -1,7 +1,6 @@
 ﻿using Deps_CleanArchitecture.Infrastructure.Data;
 using Deps_CleanArchitecture.Infrastructure.Identity;
 using Deps_CleanArchitecture.SharedKernel.Util;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,16 +8,9 @@ namespace Deps_CleanArchitecture.Infrastructure
 {
     public static class StartupSetup
     {
-        public static void AddDbContext(this IServiceCollection services) {
+        public static void AddDbContext(this IServiceCollection services) =>
             services.AddDbContext<AppDbContext>(options =>
-                options.UseNpgsql(AmbienteUtil.GetValue("POSTGRES_CONNECTION")));
-            services.AddDbContext<IdentityContext>(options =>
-                options.UseNpgsql(AmbienteUtil.GetValue("POSTGRES_CONNECTION")));
-            
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<IdentityContext>()
-                .AddDefaultTokenProviders();
-        }
+                options.UseNpgsql(AmbienteUtil.GetValue("POSTGRES_CONNECTION"))); // will be created in web project root
 
         public static void ConfigureJwt(this IServiceCollection services) => JwtStartupSetup.RegisterJWT(services);
     }
